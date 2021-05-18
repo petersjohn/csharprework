4a3f470b0b39c402a453e7102f2b86184d3a2f1a
John Peters <petersjohn0109@gmail.com>
2021-05-18 19:39:27 -0400

    6.03B Challenge

---
+++ 03_Types_Challenge/03_Types_Challenge/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03_Types_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthYear;
+            int test1 = 320000;
+            int birhtMonth;
+
+            string nameFirst= "John";
+            string yob = "1974";
+         
+            bool earthRound = true;
+            bool earthFlat;
+
+            float mashTemp = 151.3f;
+            float fermTemp;
+
+            double piVal = 3.14159265359;
+            double sqroot;
+
+            decimal retirement = 1.34m;
+            decimal dogeval;
+
+
+
+            /*string cnvtstr = birthYear.ToString(); This method seems to be reserved for the console output?*/
+            birthYear = Int32.Parse(yob);
+            Console.WriteLine("I was born in {0}.", birthYear);
+                    
+            Console.ReadLine();
+
+        }
+    }
+}
