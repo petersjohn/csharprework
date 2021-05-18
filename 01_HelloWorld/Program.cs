61fc6589ff1788a76ed30d5246f737db19729d4d
John Peters <petersjohn0109@gmail.com>
2021-05-18 09:04:58 -0400

    first commit

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey there how are you doing?");
+            Console.WriteLine("I'm good, how are you?");
+            Console.WriteLine("I'm fanstastic! I'm learning how to cod!");
+            Console.WriteLine("Wow! That's great!");
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ 02_variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string firstName = "Paul";
+
+            Console.WriteLine(firstName);
+            //1     //2 
+            string lastName = "Johnson";
+            int birthYear = 1990; //not initialized
+                            //3
+            Console.WriteLine(lastName);
+            //Console.WriteLine(birthYear);
+
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
