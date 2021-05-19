766130d7b99c4a50962a40bf82a0bbee492de65e
John Peters <petersjohn0109@gmail.com>
2021-05-18 22:01:34 -0400

    committing thru 6.7

---
+++ 07_conditionals_ternary_expressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07_conditionals_ternary_expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ternary expressions are conditionals that take 3 args, and evaluates a boolean to return a ...whatever
+            int number = 10;
+
+            //if else type a thing here
+            if(number == 7)
+            {
+                Console.WriteLine("The number is 7.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not 7");
+            }
+            string response = ((number == 7) ? "The number is 7." : "The number is not 7");
+            Console.WriteLine(response);
+        }
+    }
+}
