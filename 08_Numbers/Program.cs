43f68d93a51163d0a9fd4bd12421ff893872f34a
John Peters <petersjohn0109@gmail.com>
2021-05-18 22:17:08 -0400

    6.08

---
+++ 08_Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal bill = 54.23m;
+            decimal tip = 12;
+
+            decimal percentage = tip / bill;
+
+            Console.WriteLine(percentage);
+            Console.WriteLine(Math.Round(percentage, 2));
+            Console.WriteLine(Math.Round((percentage * 100), 2));
+            Console.WriteLine("Tip Amount is $" + (Math.Round((percentage * bill), 2)));
+
+            Console.ReadLine();
+        }
+    }
+}
