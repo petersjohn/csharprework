39a2e8993e0bebfed06387d962be77c357ec6630
John Peters <petersjohn0109@gmail.com>
2021-05-18 20:00:05 -0400

    6.04 Practice

---
+++ 04_Operators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 2);
+            Console.WriteLine(10 - 6);
+            Console.WriteLine(2 * 3);
+            Console.WriteLine(10 / 2);
+
+            //mod operator, similar to mod() in CCL 
+
+            Console.WriteLine(10 % 3);
+            Console.WriteLine(9 % 2);
+            Console.WriteLine(100 % 22);
+            Console.WriteLine(13 % 12);
+            
+        }
+    }
+}
